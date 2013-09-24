8bb1f161210925a062463d1c5cbb383b0dfa65ca
root <root@ies.ies>
2013-09-24 10:05:11 +0100

    new more files

---
+++ Unidad1/test2.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+# encoding: UTF-8
+
+a=ARGV.first
+puts a
+
+if a=="crear" then
+
+	system("mkdir curso2")
+	system("mkdir curso2/add")
+	system("mkdir curso2/sgy")
+	system("mkdir curso2/srd")
+	system("chmod 700 curso2/*")
+	puts "Carpetas Creadas!!"
+
+elsif a=="eliminar" then
+
+	system("rm -r curso2")
+	puts "Carpetas Eliminadas!!"
+	
+else
+	puts "No entiendo! <#{a}>!"
+end
